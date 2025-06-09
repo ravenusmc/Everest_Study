4e1b576fa6450c8a1d49799c3e74fbd2ac48b564
ravenusmc <mcuddy77@gmail.com>
2025-06-09 16:49:36 -0400

    built webscraping part and csv file

--- server/web_scraping/main.py
+++ server/web_scraping/main.py
@@ -1,2 +1,26 @@
 # This file will web scrape the data that I need and place it into a CSV file. 
 # Site to scrape: https://en.wikipedia.org/wiki/List_of_people_who_died_climbing_Mount_Everest
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class Scrape():
+
+  def scrape_site(self):
+    url = 'https://en.wikipedia.org/wiki/List_of_people_who_died_climbing_Mount_Everest'
+    # Making the request
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Find all tables on the page
+    tables = soup.find_all('table', {'class': 'wikitable'}) 
+    for i, table in enumerate(tables[:2]):
+      df = pd.read_html(str(table))[0]
+    # Assuming the first table is correct, convert to DataFrame
+    deaths_df = pd.read_html(str(tables[0]))[0]
+    # Save to CSV
+    deaths_df.to_csv('everest_deaths.csv', index=False)
+
+test_obj = Scrape()
+test_obj.scrape_site()
+  
