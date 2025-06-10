e013e94c41aa62561a4f83879c08f6c5e3d5f34a
ravenusmc <mcuddy77@gmail.com>
2025-06-10 16:48:19 -0400

    started work on jumbotron for landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <JumbotronComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JumbotronComponent from './components/landing/JumbotronComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JumbotronComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
