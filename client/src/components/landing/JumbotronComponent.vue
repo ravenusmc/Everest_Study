7c4ee53a9053b11ce99b732007d2b9e2ab19f5ad
ravenusmc <mcuddy77@gmail.com>
2025-06-13 15:50:24 -0400

    minor change

--- client/src/components/landing/JumbotronComponent.vue
+++ client/src/components/landing/JumbotronComponent.vue
@@ -2,7 +2,7 @@
     <div>
       <div class="jumbotron">
         <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p class="lead">content or information.</p>
         <hr>
       </div>
     </div>
