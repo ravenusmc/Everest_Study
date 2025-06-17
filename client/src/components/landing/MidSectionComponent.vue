5c7f4cefaf40476bfecaed671e2b358cc43ba67e
ravenusmc <mcuddy77@gmail.com>
2025-06-17 11:38:08 -0400

    finished midsection on landing page

--- client/src/components/landing/MidSectionComponent.vue
+++ client/src/components/landing/MidSectionComponent.vue
@@ -11,6 +11,16 @@
             we started talking about all the deaths on it. I happened to pull up a wikipedia page on Everest and it had data 
             on the amount of people who died there. I then told my coworker that I had an idea for my next project! 
           </p>
+          <p>
+            So, like my other projects, I work with a data set and build graphs from that dataset where one will be able to 
+            visualize the data. I do think that there will be some basic graphs - like top 5 states where people came from 
+            who died, top 5 reasons for deaths. The dataset that I have has about 346 points of data in it. I don't know if 
+            that's enough to do more complex analysis with it. I will be asking Chat GPT that question though. 
+          </p>
+          <p>
+            I now want to briefly mention how I got the data. As I said, it comes from a wikipedia page. I webscraped that page, 
+            pulled the data into a CSV file. This then allowed me to analyze the data. 
+          </p>
         </div>
       </div>
     </div>
@@ -31,5 +41,10 @@
   background: url("../../assets/images/Everest_line.jpg") center/cover no-repeat;
   height: 800px;
   margin-right: 5%;
+}
+
+.text-div {
+  font-size: 1.3rem;
+  line-height: 2.3rem;
 }
 </style>
