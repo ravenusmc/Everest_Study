c29dc7aec1a71d73d64de163c2093ceb900fb562
ravenusmc <mcuddy77@gmail.com>
2025-07-08 16:54:52 -0400

    built deaths by age group graph

--- client/src/components/graphs/DeathsGraph.vue
+++ client/src/components/graphs/DeathsGraph.vue
@@ -48,7 +48,98 @@ export default {
         .append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
+      
+      // Add Y axis
+      let y = d3
+        .scaleLinear()
+        .domain([0, d3.max(this.deathsByAge, (d) => d[1])])
+        .range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
 
+      // Create a tooltip div
+      let tooltip = d3
+        .select(this.$refs.deathsGraph)
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("position", "absolute");
+    
+        // Tooltip functions
+      let showTooltip = function (event, d) {
+        tooltip
+          .style("opacity", 1)
+          .html("Age Group: " + d[0] + "<br>Number of Deaths: " + d[1])
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 10 + "px");
+      };
+
+      let moveTooltip = function (event) {
+        tooltip
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 10 + "px");
+      };
+
+      let hideTooltip = function () {
+        tooltip.style("opacity", 0);
+      };
+
+       // Add bars
+      let bars = svg
+        .selectAll("rect")
+        .data(this.deathsByAge);
+      
+      // Enter new bars
+      bars
+        .enter()
+        .append("rect")
+        .attr("x", (d) => x(d[0]))
+        .attr("y", height) // Initial position at the bottom of the chart
+        .attr("width", x.bandwidth())
+        .attr("height", 0) // Initial height 0 (so it grows with the animation)
+        .attr("fill", "#0B90CA")
+        .on("mouseover", showTooltip)
+        .on("mousemove", moveTooltip)
+        .on("mouseleave", hideTooltip)
+        .transition() // Apply transition for the animation
+        .duration(1500)
+        .attr("y", (d) => y(d[1])) // Final Y position
+        .attr("height", (d) => height - y(d[1])); // Final height after transition
+      
+      // Add X axis label
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10) // Adjusted y position to be within the SVG
+        .attr("font-size", "12px")
+        .attr("font-weight", "bold")
+        .text("Age Group");
+      
+      // Add Y axis label
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 20)
+        .attr("font-size", "12px")
+        .attr("font-weight", "bold")
+        .text("Death Count");
+      
+      // Add title
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", -margin.top / 2 + 10) // Adjusted y position to be within the SVG
+        .attr("font-size", "16px")
+        .attr("font-weight", "bold")
+        .text("Deaths by Age Group");
 
     }
   } 
