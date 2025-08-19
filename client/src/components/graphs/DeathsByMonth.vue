d75ecd43d11805d08aad55eafaea8a3d9d056d71
ravenusmc <mcuddy77@gmail.com>
2025-08-19 16:36:01 -0400

    working on deaths by months graph

--- client/src/components/graphs/DeathsByMonth.vue
+++ client/src/components/graphs/DeathsByMonth.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-// import * as d3 from "d3";
+import * as d3 from "d3";
 import { mapGetters } from "vuex";
 
 export default {
@@ -29,10 +29,139 @@ export default {
     this.buildDeathByMonthGraph();
   },  
   methods: {
+    //...mapActions("datapage", ["getDataForDrillDown"]),
+    buildDeathByMonthGraph() {
+      // Clear previous SVG elements
+      d3.select(this.$refs.DeathsByMonthGraph).select("svg").remove();
+
+      // set the dimensions and margins of the graph
+      let margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      let width = 460 - margin.left - margin.right;
+      let height = 400 - margin.top - margin.bottom;
+
+      // append the svg object to the div
+      let svg = d3
+        .select(this.$refs.DeathsByMonthGraph)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Add X axis
+      let x = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(this.deathsByMonth.map((d) => d[0]))
+        .padding(0.2);
+      svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+      
+      // Add Y axis
+      let y = d3
+        .scaleLinear()
+        .domain([0, d3.max(this.deathsByMonth, (d) => d[1])])
+        .range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
+
+            // Create a tooltip div
+      let tooltip = d3
+        .select(this.$refs.DeathsByMonthGraph)
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("position", "absolute");
+      
+      // Tooltip functions
+      let showTooltip = function (event, d) {
+        tooltip
+          .style("opacity", 1)
+          .html("Month: " + d[0] + "<br>Number of Deaths: " + d[1])
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 10 + "px");
+      };
+
+      let moveTooltip = function (event) {
+        tooltip
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 10 + "px");
+      };
+
+      let hideTooltip = function () {
+        tooltip.style("opacity", 0);
+      };
+
+      // Add bars
+      let bars = svg
+        .selectAll("rect")
+        .data(this.deathsByMonth);
+      
+      // Enter new bars
+      bars
+        .enter()
+        .append("rect")
+        .attr("x", (d) => x(d[0]))
+        .attr("y", height) // Initial position at the bottom of the chart
+        .attr("width", x.bandwidth())
+        .attr("height", 0) // Initial height 0 (so it grows with the animation)
+        .attr("fill", "#121212")
+        .on("click", async (event, d) => {
+          await this.handleBarClick(d);
+        })
+        .on("mouseover", showTooltip)
+        .on("mousemove", moveTooltip)
+        .on("mouseleave", hideTooltip)
+        .transition() // Apply transition for the animation
+        .duration(1500)
+        .attr("y", (d) => y(d[1])) // Final Y position
+        .attr("height", (d) => height - y(d[1])); // Final height after transition
+    },
   },
 }
 
 </script>
 
 <style scoped>
+#popup {
+  z-index: 1000;
+  display: none;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  border: 1px solid #ccc;
+  padding: 20px;
+  max-width: 90vw;
+  max-height: 80vh;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.popup-scroll {
+  max-height: 60vh;
+  overflow-y: auto;
+  margin-bottom: 10px;
+}
+
+#popup table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#popup th, #popup td {
+  padding: 8px;
+  text-align: left;
+}
+
+#popup th {
+  background-color: #f2f2f2;
+}
 </style>
